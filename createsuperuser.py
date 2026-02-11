09a87bb305e4b20cbc05cdde31163428617e1199
erland-wt <erlandwidyatamaka05@gmail.com>
2026-02-11 12:22:16 +0700

    Force migrate and loaddata during build

---
+++ createsuperuser.py
@@ -0,0 +1,11 @@
+import os
+import django
+from django.contrib.auth import get_user_model
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings') # Sesuaikan nama 'backend'
+django.setup()
+
+User = get_user_model()
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'admin@example.com', 'password123')
+    print("Superuser created!")
