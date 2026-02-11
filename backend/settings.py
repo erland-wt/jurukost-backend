0c07769db7bfc3ca7e1991e47d5135f690c6cc03
erland-wt <erlandwidyatamaka05@gmail.com>
2026-02-11 10:24:56 +0700

    Add missing whitenoise library

--- backend/settings.py
+++ backend/settings.py
@@ -143,6 +143,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Konfigurasi GDAL untuk Windows
 if os.name == 'nt':
